6590aab6757b7d797e66c3637e7baf5adc6e145d
Sepehr Razmyar <69770592+Null3rror@users.noreply.github.com>
2023-08-07 01:32:56 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,3 +4,7 @@ if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 . "$DIR/firewall-init.sh"
 . "$DIR/fail2ban-init.sh"
 . "$DIR/sshd_config-init.sh"
+sudo rm $DIR/color-adder.sh
+sudo rm $DIR/firewall-init.sh
+sudo rm $DIR/fail2ban-init.sh
+sudo rm $DIR/sshd_config-init.sh
